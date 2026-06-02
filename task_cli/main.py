13ef9c13c1bd6c06f16b1a46f8f1ee64f931a5df
Андрій Рубан <dombldor15@gmail.com>
2026-06-02 22:51:12 +0300

    Added funcionality for deleting and list showing

--- task_cli/main.py
+++ task_cli/main.py
@@ -21,16 +21,29 @@ def main(command: str):
     except (FileNotFoundError, json.JSONDecodeError):
         data =  []
     splitted_command = command.split()
-    if MAJOR_COMMAND == splitted_command[0] and MID_COMMANDS[0] == splitted_command[1]:
-        new_task_name = " ".join(splitted_command[2:])
-        data.append(Task(randint(1,10), new_task_name, "done").__dict__)
-    elif (data is not None and len(splitted_command) >= 3 and splitted_command[1] == MID_COMMANDS[1]
-          and splitted_command[2].isdigit() and any(task['id'] == int(splitted_command[2]) for task in data)):
-        match_index = next(index for index, task in enumerate(data) if task['id'] == int(splitted_command[2]))
-        data[match_index]['description'] = " ".join(splitted_command[3:])
+    command_line_length = len(splitted_command)
+    if command_line_length >= 3:
+        if MAJOR_COMMAND == splitted_command[0] and MID_COMMANDS[0] == splitted_command[1]:
+            new_task_name = " ".join(splitted_command[2:])
+            data.append(Task(randint(1,10), new_task_name, "done").__dict__)
+        elif (data is not None and splitted_command[1] in MID_COMMANDS[1:3] and splitted_command[2].isdigit() and any(task['id'] == int(splitted_command[2]) for task in data)):
+            match_index = next(index for index, task in enumerate(data) if task['id'] == int(splitted_command[2]))
+            if (splitted_command[1] == MID_COMMANDS[1]):
+                data[match_index]['description'] = " ".join(splitted_command[3:])
+            elif (splitted_command[1] == MID_COMMANDS[2]):
+                del data[match_index]
+        elif (data is not None and MAJOR_COMMAND == splitted_command[0] and MID_COMMANDS[3] == splitted_command[1]
+            and any(task['status'] == splitted_command[2] for task in data)):
+                for task in data:
+                    if(task['status'] == splitted_command[2]):
+                        print(task)
+    elif command_line_length == 2:
+        if MAJOR_COMMAND == splitted_command[0] and MID_COMMANDS[3] == splitted_command[1]:
+            for task in data:
+                print(task)
+
     process_file_with_mode(FILE_NAME, data)
 
 def process_file_with_mode(file_name,data):
     with open(file_name, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4)
-        print(json.dumps(data, indent=4))
