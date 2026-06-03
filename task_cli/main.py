dcec88617bbd27bff7c667d0acd77dd52589d5ef
Андрій Рубан <dombldor15@gmail.com>
2026-06-03 22:56:28 +0300

    Improved updating and creating task

--- task_cli/main.py
+++ task_cli/main.py
@@ -1,6 +1,5 @@
 import json
-from random import randint
-from task_cli.task import Task
+from task_cli.task import Task, update_task
 FILE_NAME = "task_list.json"
 MAJOR_COMMAND = "task-cli"
 MID_COMMANDS = ["add", "update", "delete", "list", "mark"]
@@ -25,11 +24,13 @@ def main(command: str):
     if command_line_length >= 3:
         if MAJOR_COMMAND == splitted_command[0] and MID_COMMANDS[0] == splitted_command[1]:
             new_task_name = " ".join(splitted_command[2:])
-            data.append(Task(randint(1,10), new_task_name, "done").__dict__)
+            new_id = 1 if not data else max(some_task['id'] for some_task in data) + 1
+            data.append(Task(new_id, new_task_name, MINOR_COMMANDS[2]).__dict__)
         elif (data is not None and splitted_command[1] in MID_COMMANDS[1:3] and splitted_command[2].isdigit() and any(task['id'] == int(splitted_command[2]) for task in data)):
             match_index = next(index for index, task in enumerate(data) if task['id'] == int(splitted_command[2]))
             if (splitted_command[1] == MID_COMMANDS[1]):
                 data[match_index]['description'] = " ".join(splitted_command[3:])
+                data[match_index]['updated_at'] = update_task()
             elif (splitted_command[1] == MID_COMMANDS[2]):
                 del data[match_index]
         elif (data is not None and MAJOR_COMMAND == splitted_command[0] and MID_COMMANDS[3] == splitted_command[1]
--- task_cli/task.py
+++ task_cli/task.py
@@ -7,17 +7,12 @@ class Task:
     created_at: str
     updated_at: str
 
-    def __init__(self, id, description, status):
-        self.id = id
+    def __init__(self, id, description, status, created_at = datetime.now().isoformat(sep=' ', timespec='seconds'), updated_at = datetime.now().isoformat(sep=' ', timespec='seconds')):
+        Task.id += 1
         self.description = description
         self.status = status
-        self.created_at = datetime.now().isoformat(sep=' ', timespec='seconds')
-        self.updated_at = datetime.now().isoformat(sep=' ', timespec='seconds')
+        self.created_at = created_at
+        self.updated_at = updated_at
 
-    def update_task(self):
-        self.updated_at = datetime.now().isoformat(sep=' ', timespec='seconds')
-
-    def update_field(self, field_name, value):
-        if hasattr(self, field_name):
-            setattr(self, field_name, value)
-            self.update_task()
+def update_task():
+        return datetime.now().isoformat(sep=' ', timespec='seconds')
